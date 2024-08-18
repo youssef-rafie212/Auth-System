90780c2f62d873c2b6c3bb823ce7e21a918adcb9
youssef-rafie212 <youssefelbosaty3@gmail.com>
2024-08-18 11:31:33 +0300

    API Keys Repo

---
+++ Infrastructure/Repositories/ApiKeysRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.RepositoryContracts;
+using Infrastructure.DB;
+
+namespace Infrastructure.Repositories
+{
+    public class ApiKeysRepository : IApiKeysRepository
+    {
+        private readonly AppDBContext _db;
+
+        public ApiKeysRepository(AppDBContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<string> CreateKey(APIKey apiKey)
+        {
+            _db.APIKeys.Add(apiKey);
+            await _db.SaveChangesAsync();
+
+            return apiKey.Key!;
+        }
+    }
+}
